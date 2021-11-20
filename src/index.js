bcc7a8ecb7a7c922f7656175e2765f149475ec2a
Thế Nghĩa <nghiask2018@gmail.com>
2021-11-20 11:39:35 +0700

    upload scss css

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ const handlebars = require('express-handlebars');
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'public')))
 //HTTP logger
 app.use(morgan('combined'))
 
 //Template engine
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+app.engine('hbs', handlebars({
+  extname : '.hbs'
+}));
+app.set('view engine', 'hbs');
 app.set('views',path.join(__dirname, 'resource\\views'));
 
 app.get('/', (req, res) => {
